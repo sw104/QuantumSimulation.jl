119e71a08ffc26a11b060d831093ca542b9a9f4c
Sam W <sw104@users.noreply.github.com>
2021-03-11 23:06:33 +0000

    Add additional options for statistics printing.

--- src/stats.jl
+++ src/stats.jl
@@ -35,55 +35,72 @@ function get_stats!(quantity::Vector)
 end
 
 """
-    get_stats_str(quantity::Vector, name::String="quantity")
+    get_stats_str(quantity::Vector, name::String="quantity"; long::Bool=true)
 
 Construct a string containing a human readable presentation of the basic
 statistics returned from `get_stats(quantity)` using the label `name`.
 
+Passing `long=false` prints the statistics on a single (line feed terminated)
+line.
+
 See also: [`get_csv_stats_str`](@ref), [`get_stats_str!`](@ref), [`get_stats`](@ref)
 """
-function get_stats_str(quantity::Vector, name::String="quantity")
-  return get_stats_str(get_stats(quantity)..., name);
+function get_stats_str(quantity::Vector, name::String="quantity"; long::Bool=true)
+  return get_stats_str(get_stats(quantity)..., name, long=long);
 end
 function get_stats_str(av::Number, max::Number, min::Number, stdv::Number,
-                       name::String="quantity")
+                       name::String="quantity"; long::Bool=true)
   # Construct string.
-  return string("Average ", name, " = ", av, '\n',
-                "Standard deviation of ", name, " = ", stdv, '\n',
-                "Maximum ", name, " = ", max, '\n',
-                "Minimum ", name, " = ", min, '\n');
+  if long
+    return string("Average ", name, " = ", av, '\n',
+                  "Standard deviation of ", name, " = ", stdv, '\n',
+                  "Maximum ", name, " = ", max, '\n',
+                  "Minimum ", name, " = ", min, '\n');
+  else
+    return string(uppercasefirst(name), " statistics:\n",
+                  "μ=", av, ", σ=", stdv, ", max=", max, ", min=", min, '\n');
+  end
 end
 
 """
-    get_stats_str!(quantity::Vector, name::String="quantity")
+    get_stats_str!(quantity::Vector, name::String="quantity"; long::Bool=false)
 
 Construct a string containing a human readable presentation of the basic
 statistics returned from `get_stats!(quantity)` using the label `name`.
 
+Passing `long=false` prints the statistics on a single (line feed terminated)
+line.
+
 See also: [`get_stats_str`](@ref), [`get_stats!`](@ref)
 """
-function get_stats_str!(quantity::Vector, name::String="quantity")
-  return get_stats_str(get_stats!(quantity)..., name);
+function get_stats_str!(quantity::Vector, name::String="quantity";
+                        long::Bool=true)
+  return get_stats_str(get_stats!(quantity)..., name, long=long);
 end
 
 """
-    get_csv_stats_str(quantity::Vector, name::String="quantity")
+    get_csv_stats_str(quantity::Vector, name::String="quantity";
+                      label::Bool=true, newline::Bool=true)
 
 Construct a string containing CSV values for the basic statistics returned from
 `get_stats(quantity)` using the label `name` as the first CSV column entry.
 
 See also: [`get_stats_str`](@ref), [`get_stats_str!`](@ref), [`get_stats`](@ref)
 """
-get_csv_stats_str(quantity::Vector, name::String="quantity") =
-  get_csv_stats_str(get_stats(quantity)..., name);
+get_csv_stats_str(quantity::Vector, name::String="quantity";
+                  label::Bool=true, newline::Bool=true) =
+  get_csv_stats_str(get_stats(quantity)..., name, label=label, newline=newline);
 function get_csv_stats_str(av::Number, max::Number, min::Number, stdv::Number,
-                           name::String="quantity")
+                           name::String="quantity"; label::Bool=true, newline::Bool=true)
   # Construct string.
-  return string(name,",",av,",",stdv,",",max,",",min,'\n');
+  return string((label ? string(name,",") : ""),
+                av,",",stdv,",",max,",",min,(newline ? '\n' : ','));
 end
-function get_csv_stats_str(av::T, max::T, min::T, stdv::T, name::String="quantity") where
+function get_csv_stats_str(av::T, max::T, min::T, stdv::T, name::String="quantity";
+                           label::Bool=true, newline::Bool=true) where
   {T<:Unitful.Quantity}
   # Construct string.
-  return string(name," / ",unit(T),",",ustrip(av),",",ustrip(stdv),",",
-                ustrip(max),",",ustrip(min),'\n');
+  return string((label ? string(name," / ",unit(T),",") : ""),
+                ustrip(av),",",ustrip(stdv),",", ustrip(max),",",ustrip(min),
+                (newline ? '\n' : ','));
 end
